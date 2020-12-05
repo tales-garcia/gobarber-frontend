87b2f06908e43a7f8c5276cd32eca22295fbe1a3
Tales Garcia <garcia.tales@gmail.com>
2020-12-04 22:31:57 -0300

    feat(login page): display login page on the screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import SignIn from './pages/SignIn';
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <>
-      <h1>Oi</h1>
+      <SignIn />
       <GlobalStyle />
     </>
   );
