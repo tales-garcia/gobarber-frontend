e9fed7242d888b6e0e940f5b2b197b05753162df
Tales Garcia <garcia.tales@gmail.com>
2020-12-09 16:53:47 -0300

    feat(signin): access api on user signin
    On signin, api is accessed, and saves the data in localstorage

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,17 +1,33 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { createContext } from "react";
+import api from '../services/api';
 
+
+interface SignInCredentials {
+    email: string;
+    password: string;
+}
 
 interface AuthContextData {
     name: string;
-    signIn(): void;
+    signIn(credentials: SignInCredentials): Promise<void>;
 }
 
 export const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 export const AuthProvider : React.FC = ({ children }) => {
-    const signIn = useCallback(() => {
-        console.log('signin')
+
+    const signIn = useCallback(async ({ email, password } : SignInCredentials) => {
+        const res = await api.post('sessions', {
+            email,
+            password
+        });
+
+        const { user, token } = res.data;
+
+        localStorage.setItem('@gobarber:token', token);
+        localStorage.setItem('@gobarber:user', JSON.stringify(user));
+
     }, []);
 
     return (
