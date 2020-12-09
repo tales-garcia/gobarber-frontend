ba0412c08441c80def9f63017e8fe95f246a2e00
Tales Garcia <garcia.tales@gmail.com>
2020-12-09 12:46:43 -0300

    feat(context): create authentication context

---
+++ src/contexts/auth.ts
@@ -0,0 +1,10 @@
+import { createContext } from "react";
+
+
+interface AuthContextData {
+    name: string;
+}
+
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+export default AuthContext;
