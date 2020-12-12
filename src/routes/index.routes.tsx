80e73ec82512e002487ebb1e67db5abcc8d5f478
Tales Garcia <garcia.tales@gmail.com>
2020-12-12 17:20:11 -0300

    feat(routes): create component that handles redirecting
    Create Route component, which redirects to dashboard if user is authenticated, and to login page if
    not

--- src/routes/index.routes.tsx
+++ src/routes/index.routes.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch } from 'react-router-dom';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import Route from './Route';
 
 const Routes: React.FC = () => (
     <BrowserRouter>
         <Switch>
             <Route path="/" exact component={SignIn} />
-            <Route path="/signup" exact component={SignUp} />
+            <Route path="/signup" exact component={SignUp} isPrivate />
         </Switch>
     </BrowserRouter>
 );
