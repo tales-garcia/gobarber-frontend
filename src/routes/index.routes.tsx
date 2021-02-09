7132870050f8806a520c9b809b7132663c5c115c
Tales Garcia <garcia.tales@gmail.com>
2021-02-09 18:25:51 -0300

    feat(routes): register reset and forgot password pages

--- src/routes/index.routes.tsx
+++ src/routes/index.routes.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch } from 'react-router-dom';
+import ForgotPassword from '../pages/ForgotPassword';
+import ResetPassword from '../pages/ResetPassword';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import Route from './Route';
@@ -9,6 +11,8 @@ const Routes: React.FC = () => (
         <Switch>
             <Route path="/" exact component={SignIn} />
             <Route path="/signup" exact component={SignUp} />
+            <Route path="/forgot" exact component={ForgotPassword} />
+            <Route path="/reset" exact component={ResetPassword} />
         </Switch>
     </BrowserRouter>
 );
