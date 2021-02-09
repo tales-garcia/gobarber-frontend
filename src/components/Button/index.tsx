f0066cb7b97c57905d61e26071fa7f54ef7ac036
Tales Garcia <garcia.tales@gmail.com>
2021-02-09 17:52:40 -0300

    feat(button component): create loading handling

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,12 @@
+import { useFormikContext } from 'formik';
 import React, { ButtonHTMLAttributes } from 'react';
 import { Container } from './styles';
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Button : React.FC<ButtonProps> = ({children, ...rest}) => <Container type="button" {...rest}>{children}</Container>;
+const Button : React.FC<ButtonProps> = ({children, ...rest}) => {
+    const { isSubmitting } = useFormikContext();
+    return (<Container type="button" {...rest}>{isSubmitting ? 'Enviando...' : children}</Container>)
+};
 
 export default Button;
