25c5eda26123b3ff57e7426adde000c083a140c6
Tales Garcia <garcia.tales@gmail.com>
2021-02-10 13:51:51 -0300

    refactor(reset password): add missing dependencies

--- src/pages/ResetPassword/index.tsx
+++ src/pages/ResetPassword/index.tsx
@@ -43,7 +43,7 @@ const ResetPassword : React.FC = () => {
                 type: 'error'
             });
         }
-    }, [createToast]);
+    }, [createToast, history, token]);
 
     return (
         <Container>
